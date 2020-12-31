06c4c53c4909830359d77aa234ddd8b8b785d607
Yi-Shin Ho <yishin.ho@access-company.com>
2020-12-31 10:59:36 +0800

    add: adjust Wheel color

--- ui/src/containers/Drawer/components/Wheel.tsx
+++ ui/src/containers/Drawer/components/Wheel.tsx
@@ -6,19 +6,22 @@ const START_DEG = -0.7
 const DEG = (2 / (COUNT * 2)) * Math.PI
 
 const PADDING = 50
-const RADIUS = 300
+const RADIUS = 150
 const WHEEL_CENTER_RADIUS = 40
+const WHEEL_OUTLINE_WIDTH = 15
+const WHEEL_OUTLINE_BORDER_WIDTH = WHEEL_CENTER_RADIUS / 10
 const WHEEL_CENTER = {
   X: PADDING + RADIUS,
   Y: PADDING + RADIUS
 }
 
-const WHEEL_BORDER_COLOR = '#ccc'
+const WHEEL_OUTLINE_COLOR = '#d21404'
+const WHEEL_OUTLINE_BORDER_COLOR = '#d21404'
 const WHEEL_COLOR_FRONT = '#ff2800'
 const WHEEL_COLOR_BACK = '#fca311'
 
-const WHEEL_OUTLINE_WIDTH = 10
-const WHEEL_OUTLINE_BORDER_WIDTH = 2
+const WHEEL_CENTER_SHADOW_BLUR = 100
+const WHEEL_CENTER_SHADOW_COLOR = '#000'
 
 const WheelBack = () => <Circle x={WHEEL_CENTER.X} y={WHEEL_CENTER.Y} radius={RADIUS} fill={WHEEL_COLOR_BACK} />
 
@@ -30,6 +33,8 @@ const WheelCenter = () => (
     fill={WHEEL_COLOR_BACK}
     stroke={WHEEL_COLOR_FRONT}
     strokeWidth={WHEEL_OUTLINE_BORDER_WIDTH}
+    shadowBlur={WHEEL_CENTER_SHADOW_BLUR}
+    shadowColor={WHEEL_CENTER_SHADOW_COLOR}
   />
 )
 
@@ -39,34 +44,31 @@ const WheelFront = () => {
   return (
     <>
       {range.map((_, idx) => (
-        <WheelFrontArc key={idx} start={START_DEG + (idx * 2 + 1) * DEG} />
+        <WheelFrontArc key={idx} start={START_DEG + idx * 2 * DEG} />
       ))}
     </>
   )
 }
 
 const WheelFrontArc = ({ start }: { start: number }) => {
-  const angle = {
-    start: start,
-    end: start + DEG
-  }
+  const startAngle = start
+  const endAngle = start + DEG
+
   return (
-    <>
-      <Shape
-        sceneFunc={(context, shape) => {
-          context.beginPath()
-          context.moveTo(WHEEL_CENTER.X, WHEEL_CENTER.Y)
-          context.arc(WHEEL_CENTER.X, WHEEL_CENTER.Y, RADIUS, angle.start, angle.end, false)
-          context.closePath()
-          context.fillStrokeShape(shape)
-        }}
-        fill={WHEEL_COLOR_FRONT}
-      />
-    </>
+    <Shape
+      sceneFunc={(context, shape) => {
+        context.beginPath()
+        context.moveTo(WHEEL_CENTER.X, WHEEL_CENTER.Y)
+        context.arc(WHEEL_CENTER.X, WHEEL_CENTER.Y, RADIUS, startAngle, endAngle, false)
+        context.closePath()
+        context.fillStrokeShape(shape)
+      }}
+      fill={WHEEL_COLOR_FRONT}
+    />
   )
 }
 
-const WheelOutline = () => {
+const WheelBackGround = () => {
   return (
     <>
       <Shape
@@ -77,8 +79,10 @@ const WheelOutline = () => {
           context.closePath()
           context.fillStrokeShape(shape)
         }}
-        stroke={WHEEL_COLOR_FRONT}
+        stroke={WHEEL_OUTLINE_BORDER_COLOR}
         strokeWidth={WHEEL_OUTLINE_BORDER_WIDTH}
+        shadowBlur={WHEEL_CENTER_SHADOW_BLUR}
+        shadowColor={WHEEL_CENTER_SHADOW_COLOR}
       />
       <Shape
         sceneFunc={(context, shape) => {
@@ -87,8 +91,10 @@ const WheelOutline = () => {
           context.closePath()
           context.fillStrokeShape(shape)
         }}
-        stroke={WHEEL_BORDER_COLOR}
+        stroke={WHEEL_OUTLINE_COLOR}
         strokeWidth={WHEEL_OUTLINE_WIDTH}
+        shadowBlur={WHEEL_CENTER_SHADOW_BLUR}
+        shadowColor={WHEEL_CENTER_SHADOW_COLOR}
       />
     </>
   )
@@ -97,10 +103,10 @@ const WheelOutline = () => {
 export default function Wheel() {
   return (
     <>
+      <WheelBackGround />
       <WheelBack />
       <WheelFront />
       <WheelCenter />
-      <WheelOutline />
     </>
   )
 }
