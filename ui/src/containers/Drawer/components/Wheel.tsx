b7fd039e0d7fb943b6916972199ece8085842bb6
Yi-Shin Ho <yishin.ho@access-company.com>
2021-01-04 17:38:47 +0800

    add: adjust Wheel layout

--- ui/src/containers/Drawer/components/Wheel.tsx
+++ ui/src/containers/Drawer/components/Wheel.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { WheelBack, WheelBackGround, WheelCenter, WheelFront, WheelNeedle } from './WheelComponents'
+import { WheelBack, WheelCenter, WheelFront, WheelNeedle } from './WheelComponents'
 
 export default function Wheel() {
   return (
     <>
-      <WheelBackGround />
       <WheelBack />
       <WheelFront />
       <WheelCenter />
