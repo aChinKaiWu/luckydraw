e78ad3a82f7a6dee1ec7c798d92c58fada634387
Eric <eric.liao@access-company.com>
2021-01-08 17:24:03 +0800

    commit for merge drawing slide

--- ui/src/config/apolloClient.ts
+++ ui/src/config/apolloClient.ts
@@ -1,8 +1,41 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, gql, InMemoryCache, makeVar } from '@apollo/client'
+// import { AwardData } from '@src/_generated_/AwardData'
+import { AwardData } from '@src/__generated__/AwardData'
+
+export const awardVar = makeVar<AwardData | undefined>(undefined)
+export const chosenOneVar = makeVar<any>({ id: '12345' })
+
+const cache: InMemoryCache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        curAward: {
+          read() {
+            return awardVar()
+          }
+        },
+        winner: {
+          read() {
+            return chosenOneVar()
+          }
+        }
+      }
+    }
+  }
+})
+
+const typeDefs = gql`
+  extend type Query {
+    curAward: Award
+    # Candidate not defined yet.
+    winner: Candidate
+  }
+`
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/api',
-  cache: new InMemoryCache()
+  cache,
+  typeDefs
 })
 
 export default client
--- ui/src/containers/Drawer/Drawer.tsx
+++ ui/src/containers/Drawer/Drawer.tsx
@@ -1,16 +1,28 @@
 import React, { useRef } from 'react'
+import { Redirect } from 'react-router-dom'
+import { gql, useQuery } from '@apollo/client'
 import Konva from 'konva'
 import { Stage } from 'react-konva'
 import { IFrame } from 'konva/types/types'
 import { Layer } from 'konva/types/Layer'
 import Wheel from './components/Wheel'
+import routes from '@src/constants/routes.ts'
+import { AwardData } from '@src/__generated__/AwardData'
 
 const ANIMATION_TIME = 10000
 const HALF_LIFE = ANIMATION_TIME / 2
 
+export const CUR_AWARD_QUERY = gql`
+  query getCurrentAward {
+    curAward @client
+  }
+`
+
 export default function Drawer() {
   const frontRef = useRef<Layer>(null)
+  const { data: currAward } = useQuery<AwardData>(CUR_AWARD_QUERY)
 
+  console.log('currAward', currAward)
   const handleWheelRotation = () => {
     const anim = new Konva.Animation((frame?: IFrame) => {
       const speed = frame ? frame.time / 500 : 0
@@ -22,12 +34,14 @@ export default function Drawer() {
     setTimeout(() => anim.stop(), ANIMATION_TIME)
   }
 
-  return (
+  return currAward ? (
     <>
       <button onClick={handleWheelRotation}>Start Rotation</button>
       <Stage width={window.innerWidth} height={window.innerHeight}>
         <Wheel frontRef={frontRef} />
       </Stage>
     </>
+  ) : (
+    <Redirect to={routes.award} />
   )
 }
